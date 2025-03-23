0ed5e29c1e428919042c4516a70240fb5f106c3a
VaL <kihwouih@gmail.com>
2025-03-23 17:06:45 +0100

    Mise à jour de Navigation.jsx pour corriger les imports

--- src/components/ui/Navigation.jsx
+++ src/components/ui/Navigation.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { motion } from 'framer-motion';
-import { 
+import {
   selectStage, 
   selectGamePhase 
 } from '../../redux/selectors/gameSelectors';
